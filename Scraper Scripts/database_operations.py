9a1ef77f5580a29a699bae665322483a4fc5fdc5
kevSweet <sweetkevindan@gmail.com>
2019-06-07 16:24:47 -0700

    refactor
    refactored to update existing entries in db

--- Scraper Scripts/database_operations.py
+++ Scraper Scripts/database_operations.py
@@ -9,35 +9,55 @@ def load_data(df):
     cursor = conn.cursor()
     # load into game_props
     if df['UserScore'][0] is None:
-        cursor.execute('INSERT INTO dbo.game_props([GameID], [GameName], [Platform], [ReleaseDate])'
-                       'VALUES (?,?,?,?)', float(df['GameID'][0]), df['GameName'][0], df['Platform'][0], df['ReleaseDate'][0])
-        cursor.execute('INSERT INTO dbo.scoring([GameID], [UserScore], [CriticScore])'
-                       'VALUES (?,?,?)', float(df['GameID'][0]), None,
-                       float(df['CriticScore'][0]))
+        cursor.execute('INSERT INTO dbo.game_props([GameName], [Platform], [ReleaseDate])'
+                       'VALUES (?,?,?)', df['GameName'][0], df['Platform'][0], df['ReleaseDate'][0])
+        cursor.execute('INSERT INTO dbo.scoring([UserScore], [CriticScore])'
+                       'VALUES (?,?)',  None, float(df['CriticScore'][0]))
     else:
-        cursor.execute('INSERT INTO dbo.game_props([GameID], [GameName], [Platform], [ReleaseDate])'
-                            'VALUES (?,?,?,?)',  float(df['GameID'][0]), df['GameName'][0], df['Platform'][0], df['ReleaseDate'][0])
-        cursor.execute('INSERT INTO dbo.scoring([GameID], [UserScore], [CriticScore])' 
-                            'VALUES (?,?,?)', float(df['GameID'][0]), df['UserScore'][0], float(df['CriticScore'][0]))
+        cursor.execute('INSERT INTO dbo.game_props([GameName], [Platform], [ReleaseDate])'
+                            'VALUES (?,?,?)',  df['GameName'][0], df['Platform'][0], df['ReleaseDate'][0])
+        cursor.execute('INSERT INTO dbo.scoring([UserScore], [CriticScore])' 
+                            'VALUES (?,?)',  df['UserScore'][0], float(df['CriticScore'][0]))
     conn.commit()
 
 
-# returns -1 if no game is loaded with that name, else returns the ID for that game
-# only works if game names are strictly identical
 
-def check_game_loaded(game_name):
+# returns true if the game is in the database else false
+
+def check_game_loaded(game_name, release_date):
     conn = pyodbc.connect('Driver={SQL Server};'
                           'Server=DESKTOP-OQUP3K1;'
                           'Database=metacritic;'
                           'Trusted_Connection=yes;')
     cursor = conn.cursor()
 
-    cursor.execute("SELECT GameID "
+    cursor.execute("SELECT ScoringID "
                    "FROM dbo.game_props "
-                   "WHERE GameName = \'" + game_name + "\'")
+                   "WHERE GameName = \'" + game_name + "\'"
+                   "AND ReleaseDate = \'" + str(release_date) + "\'")
     result = cursor.fetchone()
-    result = -1 if result is None else result[0]
+    result = None if result is None else result[0]
     return result
+
+
+def update_data(df, score_id):
+    conn = pyodbc.connect('Driver={SQL Server};'
+                          'Server=DESKTOP-OQUP3K1;'
+                          'Database=metacritic;'
+                          'Trusted_Connection=yes;')
+    cursor = conn.cursor()
+    # load into game_props
+    if df['UserScore'][0] is None:
+        cursor.execute("UPDATE dbo.scoring "
+                       "SET UserScore = Null ,CriticScore = {0}".format(df['CriticScore'][0]) +
+                       "WHERE ScoringID = {0}".format(score_id))
+    else:
+        cursor.execute("UPDATE dbo.scoring "
+                   "SET UserScore = {:.1f}".format(df['UserScore'][0]) +
+                    ",CriticScore = {0}".format(df['CriticScore'][0]) +
+                   "WHERE ScoringID = {0}".format(score_id))
+    conn.commit()
+
 
 def safe_cast(val, to_type, default=None):
     try:
--- Scraper Scripts/scrape.py
+++ Scraper Scripts/scrape.py
@@ -7,11 +7,9 @@ import re
 
 
 def main():
-
     # initialize variables and selenium connection
-    col_names = ['GameID', 'GameName', 'ReleaseDate', 'Platform', 'UserScore', 'CriticScore']
+    col_names = ['GameName', 'ReleaseDate', 'Platform', 'UserScore', 'CriticScore']
     # set counter for game_id generator
-    game_id_counter = 0
 
     # setup driver configuration. set wait to avoid server timeouts
     chrome_path = r"C:\Users\Kevin\Desktop\scraper_stuff\chromedriver.exe"
@@ -19,7 +17,7 @@ def main():
     driver.implicitly_wait(30)
 
     # for every years worth of data
-    for year in range(1995, 2020):
+    for year in range(1995, datetime.now().year + 1):
         url = "https://www.metacritic.com/browse/games/score/metascore/year/all/filtered?sort=desc&year_selected=" + \
               str(year)
 
@@ -51,7 +49,6 @@ def main():
                 # pull Release Date Text
                 date_str = element.find_all('span', {'class': 'data'})[1].text
                 release_date = datetime.strptime(date_str, '%b %d, %Y')
-
                 # pull User Score
                 user_score_str = element.find_all('span', {'class': 'data'})[0].text
                 if db_ops.safe_cast(user_score_str, float) is None:
@@ -62,22 +59,18 @@ def main():
                 # pull Critic Score
                 critic_score_str = element.find_all('div', {'class': 'metascore_w'})[0].text
                 critic_score = int(critic_score_str)
-
-                # checks database for existing id with same game name. if it does not exist, create a new id
-
-                game_id = db_ops.check_game_loaded(game_name)
-                # If game does not exist in data set, create new id for it and add all elements to dataframe
-
-                if game_id == -1:
-                    game_id_counter += 1
-                    df = pd.DataFrame([(game_id_counter, game_name, release_date, platform, user_score, critic_score)],
-                                      columns=col_names)
+                # generate dataframe for input into database
+                df = pd.DataFrame(
+                    [(game_name, release_date, platform, user_score, critic_score)],
+                    columns=col_names)
+                # if the game isn't already loaded into the database load it
+                score_id = db_ops.check_game_loaded(game_name, release_date)
+                if score_id is None:
                     db_ops.load_data(df)
                     df.drop(df.index[0])
+                # otherwise update the entry with a new score
                 else:
-                    df = pd.DataFrame([(game_id, game_name, release_date, platform, user_score, critic_score)],
-                                      columns=col_names)
-                    db_ops.load_data(df)
+                    db_ops.update_data(df, score_id)
                     df.drop(df.index[0])
 
         # if there are multiple page elements, iterate over every page
@@ -117,31 +110,26 @@ def main():
 
                     # Retrieves Release Date Text
                     date_str = element.find_all('span', {'class': 'data'})[1].text
-
-                    # Retrieves User Score
+                    release_date = datetime.strptime(date_str, '%b %d, %Y')
+                    # pull User Score
                     user_score_str = element.find_all('span', {'class': 'data'})[0].text
-                    # checks if user score is N/A or has a value
                     if db_ops.safe_cast(user_score_str, float) is None:
                         user_score = None
                     else:
                         user_score = float(user_score_str)
 
-                    # Retrieves Critic Score
+                    # pull Critic Score
                     critic_score_str = element.find_all('div', {'class': 'metascore_w'})[0].text
                     critic_score = int(critic_score_str)
-
-                    # checks database for existing id with same game name. if it does not exist, create a new id
-                    game_id = db_ops.check_game_loaded(game_name)
-                    if game_id == -1:
-                        game_id_counter += 1
-                        df = pd.DataFrame([(game_id_counter, game_name, date_str, platform, user_score, critic_score)],
-                                          columns=col_names)
+                    df = pd.DataFrame(
+                        [(game_name, release_date, platform, user_score, critic_score)],
+                        columns=col_names)
+                    score_id = db_ops.check_game_loaded(game_name, release_date)
+                    if score_id is None:
                         db_ops.load_data(df)
                         df.drop(df.index[0])
                     else:
-                        df = pd.DataFrame([(game_id, game_name, date_str, platform, user_score, critic_score)],
-                                          columns=col_names)
-                        db_ops.load_data(df)
+                        db_ops.update_data(df, score_id)
                         df.drop(df.index[0])
     driver.close()
 
